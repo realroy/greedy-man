1b7f66dde26041661ba451bbbda0c5e1e93a518d
realroy <roiboon.c@ku.th>
2017-06-05 04:35:26 +0700

    Add SignUp Activity

---
+++ app/src/main/java/com/example/oldroy/greedyman/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.oldroy.greedyman
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
